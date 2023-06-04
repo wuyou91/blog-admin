de1becf17e9dedbd6774927b0e8c3f20b9331ac2
wuyou <ycx1991@live.com>
2023-06-04 15:16:07 +0800

    修改更新

--- src/views/visitorCount.vue
+++ src/views/visitorCount.vue
@@ -1,71 +1,104 @@
  <template>
   <div class="article-list">
     <sub-nav></sub-nav>
-    <el-table :data="tableData" style="width: 100%" stripe >
-      <el-table-column prop="id" label="序列号" width="180" align="center"></el-table-column>
-      <el-table-column prop="ip" label="访客ip" width="180" align="center"></el-table-column>
-      <el-table-column prop="first_time" label="首次访问时间" width="180" align="center"></el-table-column>
-      <el-table-column prop="last_time" label="最后访问时间" align="center" show-overflow-tooltip></el-table-column>
-      <el-table-column prop="visit_count" label="访问总数" align="center" show-overflow-tooltip></el-table-column>
+    <el-table :data="tableData" style="width: 100%" stripe>
+      <el-table-column
+        type="index"
+        label="序号"
+        width="180"
+        align="center"
+      ></el-table-column>
+      <el-table-column
+        prop="ip"
+        label="访客ip"
+        width="180"
+        align="center"
+      ></el-table-column>
+      <el-table-column
+        prop="first_time"
+        label="首次访问时间"
+        width="180"
+        align="center"
+      ></el-table-column>
+      <el-table-column
+        prop="last_time"
+        label="最后访问时间"
+        align="center"
+        show-overflow-tooltip
+      ></el-table-column>
+      <el-table-column
+        prop="visit_count"
+        label="访问总数"
+        align="center"
+        show-overflow-tooltip
+      ></el-table-column>
     </el-table>
     <div class="pagination">
-      <el-pagination background layout="prev, pager, next" :total= total @current-change = "handleCurrentChange"></el-pagination>
+      <el-pagination
+        background
+        layout="prev, pager, next"
+        :total="total"
+        @current-change="handleCurrentChange"
+      ></el-pagination>
     </div>
     <loading v-if="showLowding"></loading>
   </div>
- </template>
+</template>
 
 <script>
-import loading from './components/loading'
-import http from '@/http'
-import subNav from './components/subNav.vue'
+import loading from "./components/loading";
+import http from "@/http";
+import subNav from "./components/subNav.vue";
 
 export default {
-  data () {
+  data() {
     return {
       showLowding: true,
       tableData: [],
       total: 0,
-      limit: 10
-    }
+      limit: 10,
+    };
   },
   created() {
-    this.getData(1,this.limit)
+    this.getData(1, this.limit);
   },
-  components:{
+  components: {
     loading,
-    subNav
+    subNav,
   },
   methods: {
-    handleClick(arg){
-      console.log(arg)
+    handleClick(arg) {
+      console.log(arg);
     },
-    handleCurrentChange(val){
-      console.log(val)
-      this.getData(val,this.limit)
+    handleCurrentChange(val) {
+      console.log(val);
+      this.getData(val, this.limit);
     },
-    async getData(page,limit){
-      const data = {page,limit}
-        http.getVisitor(data).then((res) => {
-          this.tableData = res.data.data
-          this.total = res.data.total
-          this.showLowding = false
-        },(error) => {
+    async getData(page, limit) {
+      const data = { page, limit };
+      http.getVisitor(data).then(
+        (res) => {
+          this.tableData = res.data.data;
+          this.total = res.data.total;
+          this.showLowding = false;
+        },
+        (error) => {
           this.$message.error(error.response.data);
-          if(error.response.status ===403){
-            this.$router.push('/')
+          if (error.response.status === 403) {
+            this.$router.push("/");
           }
-        })
-    } 
-  }
-}
+        }
+      );
+    },
+  },
+};
 </script>
 
 <style lang="scss" scoped>
-.article-list{
+.article-list {
   position: relative;
 }
-.pagination{
+.pagination {
   display: flex;
   justify-content: center;
   margin-top: 30px;
